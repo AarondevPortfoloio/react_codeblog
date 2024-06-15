e0f7134d2938ec05aa5c0293d7db577ad95837fa
Aaron Dixson <88754367+AarondevPortfoloio@users.noreply.github.com>
2024-06-14 23:22:13 -0400

    commiting to main

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../app/about.css'
+export default function About() {
+  return (
+    <main>
+      <h1 id="Welcome">Welcome!</h1>
+      <p>
+Join Aaron on his journey through the ever-evolving landscape of coding, where he shares practical tips, tutorials, and reflections on his latest projects. From deep dives into algorithms to real-world application development, Aaron's blog is your go-to resource
+ for mastering the art and science of coding.
+      </p>
+    </main>
+  );
+}
